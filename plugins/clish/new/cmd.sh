65a418f08976be7d92ae36a4810db560467ee052
glejeune <gregoire.lejeune@gmail.com>
2018-09-22 01:29:49 +0200

    Initial commit

---
+++ plugins/clish/new/cmd.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+NEW_CLI_NAME="$1"
+[ -n "$NEW_CLI_NAME" ] || __help --plugin "$PLUGIN_NAME" --exit 1
+NEW_CLI_PATH="$PWD/$NEW_CLI_NAME"
+shift 
+
+IMPORT_INDEX=0
+while [ "$1" ] ; do
+  case "$1" in
+    "--force" | "-f" )
+      FORCE=1 ;
+      shift ;;
+    "--path" | "-p" )
+      shift ;
+      NEW_CLI_PATH=$1
+      [ -n "$NEW_CLI_PATH" ] || __help --plugin "$PLUGIN_NAME" --exit 1 --msg "Invalid path"
+      shift ;;
+    "--repo" | "-r" )
+      shift ;
+      NEW_CLI_REPO=$1
+      shift ;;
+    "--version" | "-v" )
+      shift ;
+      NEW_CLI_VERSION=$1
+      shift ;;
+    "--import" | "-i" ) 
+      shift ;
+      IMPORT=1
+      while [ "$IMPORT" = "1" ] ; do
+        case "$1" in 
+          -*)
+            IMPORT=0
+            ;;
+          *)
+            if [ -n "$1" ] ; then
+              if [ ! -d "$CLI_PLUGINS_PATH/$1" ] ; then
+                echo "Plugin $1 is not installed" >&2
+                exit 1
+              fi
+              eval IMPORT_$IMPORT_INDEX=\$1
+              IMPORT_INDEX=$((IMPORT_INDEX + 1))
+              shift
+            else
+              IMPORT=0
+            fi
+            ;;
+        esac
+      done
+      ;;
+    *)
+      __help --plugin "$PLUGIN_NAME" --exit 1
+      shift ;;
+  esac
+done
+
+echo "Create new cli $NEW_CLI_NAME at $NEW_CLI_PATH"
+
+if [ -d "$NEW_CLI_PATH" ] && [ "z$FORCE" = "z" ] ; then
+  echo "Directory $NEW_CLI_PATH already exists! Use --force" >&2
+  exit 1
+fi
+
+echo " * Create root directory $NEW_CLI_PATH"
+mkdir -p "$NEW_CLI_PATH"
+echo " * Create $NEW_CLI_PATH/$NEW_CLI_NAME"
+cat "$CLI_ROOT_PATH/$CLI_MAIN_COMMAND" | \
+  sed \
+  -e "s/CLI_REPO=.*$/CLI_REPO=\"$(escape "$NEW_CLI_REPO")\"/" \
+  -e "s/CLI_VERSION=.*$/CLI_VERSION=\"$(escape "$NEW_CLI_VERSION")\"/" > "$NEW_CLI_PATH/$NEW_CLI_NAME"
+echo " * Create plugin directory $NEW_CLI_PATH/plugins/$NEW_CLI_NAME"
+mkdir -p "$NEW_CLI_PATH/plugins/$NEW_CLI_NAME"
+
+echo " * Initialize plugins list"
+touch "$NEW_CLI_PATH/plugins/$NEW_CLI_NAME/list.txt"
+
+eval IMPORT_$IMPORT_INDEX="init"
+IMPORT_INDEX=$((IMPORT_INDEX + 1))
+eval IMPORT_$IMPORT_INDEX="plugins"
+IMPORT_INDEX=$((IMPORT_INDEX + 1))
+eval IMPORT_$IMPORT_INDEX="help"
+for I in $(seq 0 $IMPORT_INDEX) ; do
+  P=$(eval echo \$IMPORT_"$I")
+  if [ ! -d "$NEW_CLI_PATH/plugins/$NEW_CLI_NAME/$P" ] || [ "z$FORCE" = "z1" ]  ; then
+    echo " * import plugin $P"
+    cp -r "$CLI_ROOT_PATH/plugins/$CLI_MAIN_COMMAND/$P" "$NEW_CLI_PATH/plugins/$NEW_CLI_NAME/"
+  fi
+done
